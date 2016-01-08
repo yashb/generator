858caf8269b7ee743b6ea934b4de7d77cfc13d7e
Yash <yash@speedovation.com>
2016-01-08 21:46:29 +0530

    Breaked in classes

--- generator/p.php
+++ generator/p.php
@@ -13,25 +13,56 @@ use PhpParser\Node\Stmt;
 
 define("CACHE_DIR", "_kiwicache_" . DIRECTORY_SEPARATOR );
 
+class Indexer
+{
+    private $path;
+    
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+    
+    public function scan()
+    {
+        
+        
+        $iter = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($this->path, RecursiveDirectoryIterator::SKIP_DOTS),
+        RecursiveIteratorIterator::SELF_FIRST,
+        RecursiveIteratorIterator::CATCH_GET_CHILD // Ignore "Permission denied"
+        );
+        
+        $paths = array($this->path);
+        
+        foreach ($iter as $path => $dir) {
+            echo "File: ". $path;
+            /*if ($dir->isDir()) {
+                $paths[] = $path;
+            }*/
+        }
+    }
+}
+
+
 class JsonHelper
 {
     public static function createCache()
     {
         if(!is_dir(CACHE_DIR))
-            mkdir( CACHE_DIR , 0777, true);
+        mkdir( CACHE_DIR , 0777, true);
     }
     
-
+    
     public static function writeJson($filename, $result)
     {
         $filename = CACHE_DIR . basename($filename,".php");
-
+        
         echo "Writing json file -> " .$filename.PHP_EOL ;
         
         $content = json_encode($result);
         
         echo "content: " . $content.PHP_EOL;
-
+        
         $fp = fopen($filename.'.json', 'w');
         
         fwrite($fp, $content );
@@ -51,7 +82,7 @@ class MyParserNodeVisitor extends \PhpParser\NodeVisitorAbstract
     private $className;
     private $classMethods;
     
-    function __construct($filename) 
+    function __construct($filename)
     {
         $this->filename = $filename;
         
@@ -65,7 +96,7 @@ class MyParserNodeVisitor extends \PhpParser\NodeVisitorAbstract
         
         $this->result[$this->className]['type'] = $type;
         $this->result[$this->className]['methods'] = [];
-
+        
     }
     
     public function enterNode(Node $node)
@@ -79,7 +110,7 @@ class MyParserNodeVisitor extends \PhpParser\NodeVisitorAbstract
         || $node instanceof Stmt\Function_
         
         */
-         if ( $node instanceof Stmt\Function_ )
+        if ( $node instanceof Stmt\Function_ )
         {
             array_push( $this->result['functions'], $node->name );
         }
@@ -132,8 +163,8 @@ class MyParserNodeVisitor extends \PhpParser\NodeVisitorAbstract
             
             
             $this->addItem($node->name, 'class');
-/*            $this->result['class'] = $node->name;*/
-           
+            /*            $this->result['class'] = $node->name;*/
+            
             
         }
         
@@ -189,41 +220,77 @@ class MyParserNodeVisitor extends \PhpParser\NodeVisitorAbstract
 
 
 
-$lexer = new PhpParser\Lexer(array(
-'usedAttributes' => array(
-'comments', 'startLine', 'endLine', 'startTokenPos', 'endTokenPos'
-)
-));
 
-//Create cache dir
+class MyPhpParser
+{
+    // private $lexer;
+    private $parser;
+    private $traverser;
+    private $filename;
+    
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+        
+        //Create cache dir
+        JsonHelper::createCache();
+        
+        //Init 
+        $this->initInstances();
+        
+        //parse file
+        $this->myParser();
+        
+    }
+    
+    private function initInstances()
+    {
+        $lexer = new PhpParser\Lexer ( array(
+        'usedAttributes' => array(
+        'comments', 'startLine', 'endLine', 'startTokenPos', 'endTokenPos'
+        )
+        ) ) ;
+        
+        $this->parser = (new PhpParser\ParserFactory)->create(PhpParser\ParserFactory::PREFER_PHP5, $lexer);
+        
+        
+        $visitor = new MyParserNodeVisitor($this->filename);
+        
+        $this->traverser = new PhpParser\NodeTraverser();
+        $this->traverser->addVisitor($visitor);
+    }
+    
+    private function myParser()
+    {
+        try {
+            $stmts = $this->parser->parse(file_get_contents($this->filename));
+            
+            //print_r($stmts);
+            
+            //$visitor->setTokens($lexer->getTokens());
+            $stmts = $this->traverser->traverse($stmts);
+            
+        }
+        catch (PhpParser\Error $e)
+        {
+            echo 'Parse Error: ', $e->getMessage();
+        }
+        
+        
+    }
+    
+}
 
-JsonHelper::createCache();
+
+
 
 $filename = $argv[1];
-
-$parser = (new PhpParser\ParserFactory)->create(PhpParser\ParserFactory::PREFER_PHP5, $lexer);
-
-$visitor = new MyParserNodeVisitor($filename);
-
-$traverser = new PhpParser\NodeTraverser();
-$traverser->addVisitor($visitor);
 
 
 if(empty($filename))
 exit();
 
-try {
-    $stmts = $parser->parse(file_get_contents($filename));
-    
-    //print_r($stmts);
-    
-    //$visitor->setTokens($lexer->getTokens());
-    $stmts = $traverser->traverse($stmts);
-    
-}
-catch (PhpParser\Error $e)
-{
-    echo 'Parse Error: ', $e->getMessage();
-}
+new MyPhpParser($filename);
 
 
+//getcwd()
